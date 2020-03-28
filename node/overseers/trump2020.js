cd32a56196f574b3e2d2cbe09acc56fc3653e304
Aakil Fernandes <aakilfernandes@gmail.com>
2020-03-27 21:38:20 -0400

    redeploy trump2020

--- node/overseers/trump2020.js
+++ node/overseers/trump2020.js
@@ -1,4 +1,4 @@
 "use strict";
 exports.__esModule = true;
 var pollenium_uvaursi_1 = require("pollenium-uvaursi");
-exports.trump2020 = pollenium_uvaursi_1.Uu.fromHexish('d4c7f0692cfde069e0dac86cd5492943f048f1a5');
+exports.trump2020 = pollenium_uvaursi_1.Uu.fromHexish('06c1a8e2ffe1bc030788aac7814329501de6a72e');
--- ts/overseers/trump2020.ts
+++ ts/overseers/trump2020.ts
@@ -1,2 +1,2 @@
 import { Uu } from 'pollenium-uvaursi'
-export const trump2020 = Uu.fromHexish('d4c7f0692cfde069e0dac86cd5492943f048f1a5')
+export const trump2020 = Uu.fromHexish('06c1a8e2ffe1bc030788aac7814329501de6a72e')
